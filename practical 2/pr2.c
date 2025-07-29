640931c0430e3f5fe802168072a7f4df160c9581
yatisoliyas-ce <yatisoliyas@gmail.com>
2025-07-29 10:11:46 +0530

    Add files via upload

---
+++ practical 2/pr2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+// This shows area and perimeter of the rectangular park
+{
+    int Length;
+    Length=70;
+    int Breadth;
+    Breadth=90;
+    int Area;
+    Area=Length*Breadth;
+    int Perimeter;
+    Perimeter=2*(Length+Breadth);
+    printf("The Area of the park is %d",Area);
+    printf("The Perimeter of the park is %d",Perimeter);
+    printf("This Program is done by shakshi Chaudhary 25TCE9EY");
+    return 0;
+}
